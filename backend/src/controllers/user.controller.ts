c8a8992cedb237c147be8668f016a0bbb4303aac
Emnet-tes <teshomeemnet255@gmail.com>
2025-07-06 22:23:29 +0300

    chore: add profile update

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -22,6 +22,29 @@ export const createUser = async (req: Request, res: Response) => {
   }
 };
 
+export const updateUserProfile = async (req: Request, res: Response):Promise<void> => {
+  try {
+    const { auth0Id } = req.params;
+    const { name, date, mobilePhone } = req.body;
+
+    const user = await User.findOneAndUpdate(
+      { auth0Id },
+      { name, date, mobilePhone },
+      { new: true }
+    );
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "User profile updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+};
+
 export const getUserProfile = async (req: Request, res: Response) => {
   try {
     const { auth0Id } = req.params;
--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  auth0Id: { type: String, required: true, unique: true },
-  email: { type: String },
-  name: { type: String },
-});
+const userSchema = new mongoose.Schema(
+  {
+    auth0Id: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    name: { type: String },
+    date: { type: Date },
+    mobilePhone: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("User", userSchema);
