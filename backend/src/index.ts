744313640a9f2d7964b6a364e2f3077860141149
MihretDe <mihretdesalegn98@gmail.com>
2025-07-09 12:01:17 +0300

    revert package.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,7 @@ app.use(
 );
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000; // <-- Use 10000 as default for Render compatibility
 const __dirname = path.resolve();
 console.log("NODE_ENV:", process.env.NODE_ENV);
 
